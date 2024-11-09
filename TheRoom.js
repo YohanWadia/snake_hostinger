1a0a876327680742ea5d13030902fd41c551b3da
YohanWadia <39173295+YohanWadia@users.noreply.github.com>
2024-11-09 16:18:00 +0200

    Add files via upload

---
+++ TheRoom.js
@@ -0,0 +1,11 @@
+class TheRoom {
+    constructor(id,pk) {
+        this.id=id;
+        this.pk=pk;
+        this.count;
+        this.toBsent;
+        this.ended = true;
+    }
+}
+
+module.exports = TheRoom;
